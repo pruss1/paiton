8364e461f43a524111382678ee3ccb43ce4b45ca
pruss1 <cataprussxd@gmail.com>
2025-07-16 00:05:33 -0300

    Update ej3.py

--- python/ej3.py
+++ python/ej3.py
@@ -1,13 +1,16 @@
 n=int(input("ingrese n del 1 al 10"))
 m=int(input("ingrese m del 1 al 10"))
 try:
-    archivo=open("tabla-n.txt","w")
-    for i in range(1,11):
-        archivo.write(f"{n}x{i}={n*i}\n")
-    archivo.close()
-    archivo=open("tabla-n.txt")
-    l=archivo.readlines()
-    print(l[m-1])
-    archivo.close()
+    if 1<=n<=10 and 1<=m<=10:
+        archivo=open("tabla-n.txt","w")
+        for i in range(1, 11):
+            archivo.write(f"{n}x{i}={n*i}\n")
+        archivo.close()
+        archivo=open("tabla-n.txt")
+        l=archivo.readlines()
+        print(l[m-1])
+        archivo.close()
+    else:
+        print("lelelel")
 except IOError:
     print("oooooooo")
